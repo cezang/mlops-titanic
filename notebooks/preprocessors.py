b50abb15b250bcb3aac04b3db2e69beab4074688
Cezary Angielczyk <cezary.angielczyk@gmail.com>
2025-01-05 12:24:48 +0100

    Update .gitignore to ignore __pycache__ folders

--- notebooks/preprocessors.py
+++ notebooks/preprocessors.py
@@ -5,47 +5,25 @@ class TitleExtractor(BaseEstimator, TransformerMixin):
     """
     A custom transformer for extracting titles from strings.
     """
-    def __init__(self):
-        """
-        Initialize the transformer.
-        """
-        pass
+    def __init__(self, variables=None, list_of_new_col_names=None):
+        if not isinstance(variables, list):
+            raise ValueError("variables should be a list")
+        if not isinstance(list_of_new_col_names, list):
+            raise ValueError("list_of_new_col_names should be a list")
+        self.variables = variables
+        self.list_of_new_col_names = list_of_new_col_names
     
     def fit(self, X, y=None):
-        """
-        Fit the transformer. This transformer does not need fitting, so we just return self.
-        
-        Args:
-            X (array-like): The data to fit.
-            y (array-like, optional): The target values (ignored).
-
-        Returns:
-            self
-        """
         return self
 
     def transform(self, X):
-        """
-        Transform the input data by extracting titles.
-
-        Args:
-            X (pandas col): The column to transform.
-
-        Returns:
-            pd.Series: The transformed data with extracted titles.
-        """
-        return X.apply(self._extract_title)
+        X = X.copy()
+        for feature, new_col_name in zip(self.variables, self.list_of_new_col_names):
+            X[feature] = X[feature].apply(self._extract_title)
+            X.rename(columns={feature: new_col_name}, inplace=True)
+        return X
 
     def _extract_title(self, title):
-        """
-        Extracts the title from the given string.
-
-        Args:
-            title (str): The string containing the title.
-
-        Returns:
-            str: The extracted title.
-        """
         match = re.search(r',\s*([^,]+)\.', title)
         if match:
             return match.group(1).strip()
